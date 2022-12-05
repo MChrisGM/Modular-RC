f248a05a3fb2132c3710c7a57500123a1be2dbed
MChrisGM <c.i.cristakos@gmail.com>
2022-12-05 19:10:18 +0000

    Added index.html

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express')
-const app = express()
 const videoStream = require('raspberrypi-node-camera-web-streamer');
-
-
+const fs = require('fs');
 const localtunnel = require('localtunnel');
 
+const app = express();
 
 PORT = 3000;
 
 (async () => {
   const tunnel = await localtunnel({ PORT: 3000, subdomain:'f1rc' });
-
   console.log(tunnel.url);
-
   tunnel.on('close', () => {
     // tunnels are closed
   });
@@ -26,4 +23,6 @@ videoStream.acceptConnections(app, {
     quality: 4 //lower is faster
 }, '/stream.mjpg', false);
 
+
+app.use(express.static(__dirname+'/public'));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
